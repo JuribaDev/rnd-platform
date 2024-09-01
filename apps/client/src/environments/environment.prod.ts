383c55513f733eeecf05464d21c7bf482f1adac2
Juriba <Juribaalsiari@gmail.com>
2024-09-01 04:36:24 +0300

    clean up

--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: typeof window !== 'undefined' ? window.location.origin + '/api/v1' : '${API_URL}/api/v1'
   apiUrl: 'API_URL_PLACEHOLDER/api/v1'
 };
